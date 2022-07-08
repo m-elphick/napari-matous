f819a000d02464e20a042d3d252593897e74cd15
elphicm <matous.elphick@crick.ac.uk>
2022-07-08 17:22:59 +0100

    Removing Testing

--- src/napari_matous/segment.py
+++ src/napari_matous/segment.py
@@ -4,12 +4,9 @@ from skimage.color import rgb2gray
 from napari.layers import Labels
 from magicgui import magic_factory
 from napari.layers import Image
-import matplotlib.pyplot as plt
 
 
 def segment(image):
-    fig, ax = try_all_threshold(image, figsize=(30, 15), verbose=False)
-    plt.show()
     thresh = threshold_otsu(image)
     bw = closing(image > thresh, square(3))
     return bw
@@ -17,15 +14,6 @@ def segment(image):
 
 @magic_factory(layout='vertical', call_button='Segment')
 def segment_image(image: Image) -> Labels:
-
-    print(image.data.shape)
-
-    input_arr = image.data  # np.squeeze(input.data)
-    print(input_arr.shape)
-
-    temp_idx = 2 if len(input_arr.shape) == 4 else 1
-
-    print(input_arr.shape[temp_idx:])
 
     if image.rgb:
         image = rgb2gray(image.data)
