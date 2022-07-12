e82bdbdd5043d1bf1691cf8115ed01bddcca17e9
elphicm <matous.elphick@crick.ac.uk>
2022-07-12 15:29:28 +0100

    __init__.py : Adding new tools to init file

--- src/napari_matous/__init__.py
+++ src/napari_matous/__init__.py
@@ -1,7 +1,9 @@
 __version__ = "0.0.1"
 from .segment import segment_image
-
-
+from .stardistsegment import stardist_segment_image
+from .meshfilter import load_mesh
 __all__ = (
     "segment_image",
+    "stardist_segment_image",
+    "load_mesh"
 )
