4889c00e5da093d70d7f5fa38d935c459b68504e
elphicm <matous.elphick@crick.ac.uk>
2022-07-14 12:57:29 +0100

    Adding tiff2mesh to set up files

--- src/napari_matous/__init__.py
+++ src/napari_matous/__init__.py
@@ -2,8 +2,10 @@ __version__ = "0.0.1"
 from .segment import segment_image
 from .stardistsegment import stardist_segment_image
 from .meshfilter import load_mesh
+from .tiff2mesh import tiff_2_mesh
 __all__ = (
     "segment_image",
     "stardist_segment_image",
-    "load_mesh"
+    "load_mesh",
+    "tiff_2_mesh"
 )
